7cb699e5c1fe9d171f7bdaf75004594257f8f205
Isaiah Glick <iaristotleg@gmail.com>
2018-06-14 17:34:33 -0400

    Give itr a commit

--- whowouldwin.js
+++ whowouldwin.js
@@ -1,42 +1,36 @@
 
-console.log("Hey world");
 
 $(document).ready(function() {
-  $(".heroImg").click(function() {
-    $( ".heroImg" ).removeClass( "selectedImg")
-    $(this).addClass("selectedImg");
-    alert( "Selected the Champion!" );
-  });
-  //$(".brown").click(yoMan);
-  $("#button").click(function() {
-        // alert( "Superhuman Selected. Now see what other people think!");
-
-        if (selectedImg.length == 1 )(onclick.length == "submit"){
-           alert($("#data-name").val());
-
-         };
-  });
-
-
-
-    $("#submit").click(function(){
-
-      var selectedElements = $(".selectedImg");
-      console.log(selectedElements);
-
-
-          if (selectedImg.length == 0){
-            alert("Choose a winner.");
-          };
-          if (selectedImg.length == 2){
-            alert("Only ONE can win!.");
-          };
-      });
-
+    $(".heroImg").click(function() {
+        $(".heroImg").removeClass("selectedImg")
+        $(this).addClass("selectedImg");
+        alert("Selected the Champion!");
     });
 
 
+    $("#submit").click(function() {
 
-function yoMan(name){
-  console.log("Yo man, I'm a weak little twerp who dances like a dead dolphin.");
-}
+        var selectedElements = $(".selectedImg");
+        console.log(selectedElements);
+
+
+        if (selectedElements.length == 0) {
+            alert("Choose a winner.");
+        };
+
+        if (selectedElements.length == 1) {
+            alert("Triumphant is the winner!");
+        };
+
+        if (selectedElements.length == 2) {
+            alert("Only ONE can win!.");
+        };
+    });
+
+});
+
+
+
+// function yoMan(name){
+//   console.log("Yo man, I'm a weak little twerp who dances like a dead dolphin.");
+// }
