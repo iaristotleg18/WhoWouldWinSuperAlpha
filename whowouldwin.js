a6efbe7c160b09451628ea9f262f044e0801bfe2
iaristotleg18 <iaristotleg@gmail.com>
2019-02-18 17:25:59 -0500

    Add files via upload

--- whowouldwin.js
+++ whowouldwin.js
@@ -1,85 +1,40 @@
 $(document).ready(function() {
-    // Determine which heroes are fighting
-    var heroes = selectHeroes();
-    var base1 = "http://superheroapi.com/api.php/10156085785976648/";
-    var hero1Url = base1 + heroes.hero1;
-    var hero2Url = base1 + heroes.hero2;
 
-    // Retrieve the hero data
-    $.ajax({
-      method: 'GET',
-      url: hero1Url,
-    }).done(function(data) {
-        $('.heroName.hero1').text(data.name)
-        $('.heroAlter.hero1').text(data.biography['full-name'])
-        $('.heroHeight.hero1').text(data.appearance.height[0])
-        $('.heroWeight.hero1').text(data.appearance.weight[0])
-        $('.heroGender.hero1').text(data.appearance.gender)
-        $('.heroRace.hero1').text(data.appearance.race)
-        $('.heroIntelligence.hero1').text(data.powerstats.intelligence)
-        $('.heroSpeed.hero1').text(data.powerstats.speed)
-        $('.heroStrength.hero1').text(data.powerstats.strength)
-        $('.heroDurability.hero1').text(data.powerstats.durability)
-        $('.heroPower.hero1').text(data.powerstats.power)
-        $('.heroGroup.hero1').text(data.connections.groupAffiliation)
-        $('.heroApp.hero1').text(data.biography['first-appearance'])
-        $(".heroImg.hero1").attr("src", data.image.url)
-    })
+  // Determine which heroes are fighting
+  var heroes = selectHeroes();
+  console.log(heroes);
 
-    $.ajax({
-      method: 'GET',
-      url: hero2Url,
-    }).done(function(data) {
-      $('.heroName.hero2').text(data.name)
-      $('.heroAlter.hero2').text(data.biography['full-name'])
-      $('.heroHeight.hero2').text(data.appearance.height[0])
-      $('.heroWeight.hero2').text(data.appearance.weight[0])
-      $('.heroGender.hero2').text(data.appearance.gender)
-      $('.heroRace.hero2').text(data.appearance.race)
-      $('.heroIntelligence.hero2').text(data.powerstats.intelligence)
-      $('.heroSpeed.hero2').text(data.powerstats.speed)
-      $('.heroStrength.hero2').text(data.powerstats.strength)
-      $('.heroDurability.hero2').text(data.powerstats.durability)
-      $('.heroPower.hero2').text(data.powerstats.power)
-      $('.heroGroup.hero2').text(data.connections.groupAffiliation)
-      $('.heroApp.hero2').text(data.biography['first-appearance'])
-      $(".heroImg.hero2").attr("src", data.image.url  )
-    })
+  loadHero(heroes.hero1, "hero1");
+  loadHero(heroes.hero2, "hero2");
+
     // When the user selects a hero
-    $(".heroImg").click(function() {
-        $(".heroImg").removeClass("selectedImg")
-        $("#submit").prop ("disabled", false);
+    $(".heroWrapper").click(function() {
+        $(".heroWrapper").removeClass("selectedImg");
+        // $("#submit").prop("disabled", false);
         $(this).addClass("selectedImg");
-        alert("Selected the Champion!");
+        document.location.reload()
     });
 
-    // When the user clicks the 'submit' button
-    $("#submit").click(function() {
-        var selectedElements = $(".selectedImg");
-        console.log(selectedElements);
-
-        if (selectedElements.length == 0) {
-          $("#submit").prop ("disabled", true);
-          alert("CHOOSE A DAMN HERO! IT DOESN'T MATTER IF ITS RA'S AL-GHUL OR THOR!");
-        };
-
-        if (selectedElements.length == 1) {
-            $("#submit").prop("disabled", false);
-            var chosenHero = selectedElements[0];
-
-            alert( $(chosenHero).data("name"));
-            document.location.reload()
-        };
-
-        if (selectedElements.length == 2) {
-            alert("Only ONE can win!.");
-        };
+    $("img").on("error", function(){
+      var existingHeroIds = $(".heroImg").map(function(){
+        return $(this).attr('hero-id');
+      });
+      console.log(existingHeroIds);
+      var heroThatFailed = $(this); //element that broke
+      var imgId = heroThatFailed.attr('id');
+      loadHero(selectHero(existingHeroIds), imgId);
     });
 });
 
-function displayHeroData(heroData){
-  console.log(heroData);
-};
+var selectHero = function(existingHeroIds){
+  heroId = Math.floor(Math.random() * 730) + 1;
+
+  while(heroId == existingHeroIds[0] || heroId == existingHeroIds[1]) {
+    heroId = Math.floor(Math.random() * 730) + 1;
+  }
+
+  return heroId;
+}
 
 //Actual business for the determining
 var selectHeroes = function() {
@@ -98,3 +53,29 @@ var selectHeroes = function() {
     hero2: hero2
   }
 }
+
+function loadHero(heroId, heroClass) {
+  var base = "http://superheroapi.com/api.php/10156085785976648/";
+  var heroUrl = base + heroId;
+
+  $.ajax({
+    method: 'GET',
+    url: heroUrl,
+  }).done(function(data) {
+      $('.heroName.' + heroClass).text(data.name)
+      $('.heroAlter.' + heroClass).text(data.biography['full-name'])
+      $('.heroHeight.' + heroClass).text(data.appearance.height[0])
+      $('.heroWeight.' + heroClass).text(data.appearance.weight[0])
+      $('.heroRace.' + heroClass).text(data.appearance.race)
+      $('.heroIntelligence.' + heroClass).text(data.powerstats.intelligence)
+      $('.heroSpeed.' + heroClass).text(data.powerstats.speed)
+      $('.heroStrength.' + heroClass).text(data.powerstats.strength)
+      $('.heroDurability.' + heroClass).text(data.powerstats.durability)
+      $('.heroPower.' + heroClass).text(data.powerstats.power)
+      $('.heroGroup.' + heroClass).text(data.connections.groupAffiliation)
+      $('.heroApp.' + heroClass).text(data.biography['first-appearance'])
+      $(".heroImg." + heroClass).attr("hero-id", heroId)
+      $(".heroImg." + heroClass).attr("src", data.image.url)
+      $(".heroWrapper." + heroClass).attr("data-name", data.name)
+  });
+};
