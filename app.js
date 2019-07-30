ce74ff0361942c731ef0e1ee7593fdc505e9f156
Isaiah Glick <iaristotleg@gmail.com>
2019-07-30 18:49:19 -0400

    most recent version of app. working with external database

---
+++ app.js
@@ -0,0 +1,86 @@
+// var http = require('http');
+//
+var handleVisitor = function(req, res) {
+  res.send('Nylon potato chimpanzee dolphin murder raven');
+}
+//
+// var server = http.createServer(handleVisitor);
+// server.listen(8080);
+// console.log("See you in the future");
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: '67.205.151.220',
+  user: 'WinData',
+  password: 'K!FVzh2@qso8ZALEDkzI67RFb$Woyo',
+  database: 'Superheroes'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Entered the Database. Incoming transmission");
+})
+
+connection.query('SELECT * FROM Hero_Name', (err,rows) => {
+  if (err) throw err;
+
+  console.log(rows);
+})
+
+const express = require('express')
+var bodyParser = require("body-parser");
+var cors = require('cors')
+const app = express()
+const port = process.env.PORT;
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', handleVisitor)
+
+app.get("/ad", function (req, res){
+  res.send ("I'm Bob Needley, and I'd like you to come to Bob's Subsidized Fried Chicken! It's called that because it sounds cheaper.")
+})
+
+app.post("/selectHero", function (req, res){
+  console.log(req.body.winnerId)
+  console.log(req.body.loserId);
+  if (req.body.winnerId && req.body.loserId) {
+    // insert these ids into the db
+    const battle = {
+      winner_id: req.body.winnerId,
+      loser_id: req.body.loserId
+    };
+    // INSERT INTO Hero_Win SET winner_id = 5, loser_id = 10
+    connection.query('INSERT INTO Hero_Win SET ?', battle, (err, res) => {
+      if(err) throw err;
+      console.log('Data loaded and processed.')
+    })
+  }
+
+  res.send ("Done and done and done")
+})
+
+app.get("/best", function (req, res){
+  connection.query("SELECT winner_id, COUNT(*) FROM Hero_Win GROUP BY winner_id ORDER BY COUNT(*) DESC limit 1", function (err, result, fields){
+    res.send(result);
+    console.log(result);
+  });
+});
+
+app.get("/worst", function (req, res){
+  connection.query("select loser_id, count(*) from Hero_Win group by loser_id order by count(*) DESC limit 1", function (err, result, fields){
+    res.send(result);
+    console.log(result);
+  });
+});
+
+app.get("/percent", function (req, res){
+  connection.query("SELECT SUM(winner_id = ?)/COUNT(*) FROM Hero_Win WHERE winner_id = ? or loser_id = ?", [req.query.heroId,req.query.heroId,req.query.heroId], function (err, result, fields){
+    console.log(this.sql);
+    res.send(result);
+    console.log(result);
+  });
+});
+
+app.listen(port, () => console.log(`See you in the future`))
--- whowouldwin.js
+++ whowouldwin.js
@@ -1,88 +1,89 @@
 $(document).ready(function() {
-    // Determine which heroes are fighting
-    var heroes = selectHeroes();
-    var base1 = "http://superheroapi.com/api.php/10156085785976648/";
-    var hero1Url = base1 + heroes.hero1;
-    var hero2Url = base1 + heroes.hero2;
-
-    // Retrieve the hero data
+  // Get hero leaderboarf
+  // current winner
+  $.ajax({
+    method: 'get',
+    url: 'http://localhost:8080/best',
+    contentType: "application/json"
+  }).done(function(data){
     $.ajax({
-      method: 'GET',
-      url: hero1Url,
-    }).done(function(data) {
-        $('.heroName.hero1').text(data.name)
-        $('.heroAlter.hero1').text(data.biography['full-name'])
-        $('.heroHeight.hero1').text(data.appearance.height[0])
-        $('.heroWeight.hero1').text(data.appearance.weight[0])
-        $('.heroGender.hero1').text(data.appearance.gender)
-        $('.heroRace.hero1').text(data.appearance.race)
-        $('.heroIntelligence.hero1').text(data.powerstats.intelligence)
-        $('.heroSpeed.hero1').text(data.powerstats.speed)
-        $('.heroStrength.hero1').text(data.powerstats.strength)
-        $('.heroDurability.hero1').text(data.powerstats.durability)
-        $('.heroPower.hero1').text(data.powerstats.power)
-        $('.heroGroup.hero1').text(data.connections.groupAffiliation)
-        $('.heroApp.hero1').text(data.biography['first-appearance'])
-        $(".heroImg.hero1").attr("src", data.image.url)
-        $(".heroImg.hero1").attr("data-name", data.name)
-
+      method: 'get',
+      url: baseUrl + data[0].winner_id,
+    }).done(function(data){
+      console.log(data.name)
+      $('.Champion').text(data.name)
     })
+  });
 
+  // current loser
+  $.ajax({
+    method: 'get',
+    url: 'http://localhost:8080/worst',
+    contentType: "application/json"
+  }).done(function(data){
     $.ajax({
-      method: 'GET',
-      url: hero2Url,
-    }).done(function(data) {
-      $('.heroName.hero2').text(data.name)
-      $('.heroAlter.hero2').text(data.biography['full-name'])
-      $('.heroHeight.hero2').text(data.appearance.height[0])
-      $('.heroWeight.hero2').text(data.appearance.weight[0])
-      $('.heroGender.hero2').text(data.appearance.gender)
-      $('.heroRace.hero2').text(data.appearance.race)
-      $('.heroIntelligence.hero2').text(data.powerstats.intelligence)
-      $('.heroSpeed.hero2').text(data.powerstats.speed)
-      $('.heroStrength.hero2').text(data.powerstats.strength)
-      $('.heroDurability.hero2').text(data.powerstats.durability)
-      $('.heroPower.hero2').text(data.powerstats.power)
-      $('.heroGroup.hero2').text(data.connections.groupAffiliation)
-      $('.heroApp.hero2').text(data.biography['first-appearance'])
-      $(".heroImg.hero2").attr("src", data.image.url)
-      $(".heroImg.hero2").attr("data-name", data.name)
+      method: 'get',
+      url: baseUrl + data[0].loser_id,
+    }).done(function(data){
+      console.log(data.name)
+      $('.Unlucky').text(data.name)
     })
+  });
+
+
+  var heroes = selectHeroes();
+  console.log(heroes);
+
+  console.log("I Am John Iron Man");
+
+  loadHero(heroes.hero1, "hero1");
+  loadHero(heroes.hero2, "hero2");
 
     // When the user selects a hero
-    $(".heroImg").click(function() {
-        $(".heroImg").removeClass("selectedImg");
-        $("#submit").prop("disabled", false);
+    $(".heroWrapper").click(function() {
+      // close the fists
+        $(".heroWrapper").removeClass("selectedImg");
+        // open the clicked fist
         $(this).addClass("selectedImg");
+        // post their selection to backend
+        var victorId = $(this).attr("data-heroId")
+        var loserId = $(".heroWrapper:not(.selectedImg)").attr("data-heroId")
+
+        // ajax post request
+        $.ajax({
+          method: 'post',
+          url: 'http://localhost:8080/selectHero',
+          contentType: "application/json",
+          data: JSON.stringify({
+            winnerId: victorId,
+            loserId: loserId
+          })
+        }).done(function(data){
+          console.log(data)
+        });
+      document.location.reload()
     });
 
-    // When the user clicks the 'submit' button
-    $("#submit").click(function() {
-        var selectedElements = $(".selectedImg");
-        console.log(selectedElements);
-
-        if (selectedElements.length == 0) {
-          $("#submit").prop ("disabled", true);
-          alert("CHOOSE A DAMN HERO! IT DOESN'T MATTER IF ITS RA'S AL-GHUL OR THOR!");
-        };
-
-        if (selectedElements.length == 1) {
-            $("#submit").prop("disabled", false);
-            var chosenHero = selectedElements[0];
-
-            alert( $(chosenHero).data("name"));
-            document.location.reload()
-        };
-
-        if (selectedElements.length == 2) {
-            alert("Only ONE can win!.");
-        };
+    $(".heroImg").on("error", function(){
+      var existingHeroIds = $(".heroImg").map(function(){
+        return $(this).attr('data-heroId');
+      });
+      var heroThatFailed = $(this); //element that broke
+      var imgId = heroThatFailed.attr('id');
+      loadHero(selectHero(existingHeroIds), imgId);
     });
+
 });
 
-function displayHeroData(heroData){
-  console.log(heroData);
-};
+var selectHero = function(existingHeroIds){
+  heroId = Math.floor(Math.random() * 730) + 1;
+
+  while(heroId == existingHeroIds[0] || heroId == existingHeroIds[1]) {
+    heroId = Math.floor(Math.random() * 730) + 1;
+  }
+
+  return heroId;
+}
 
 //Actual business for the determining
 var selectHeroes = function() {
@@ -101,3 +102,43 @@ var selectHeroes = function() {
     hero2: hero2
   }
 }
+
+var baseUrl = "http://superheroapi.com/api.php/10156085785976648/";
+
+function loadHero(heroId, heroClass) {
+  var heroUrl = baseUrl + heroId;
+
+  $.ajax({
+    method: 'GET',
+    url: heroUrl,
+  }).done(function(data) {
+    console.log(data);
+      $('.heroName.' + heroClass).text(data.name)
+      $('.heroAlter.' + heroClass).text(data.biography['fullName'])
+      $('.heroHeight.' + heroClass).text(data.appearance.height[0])
+      $('.heroWeight.' + heroClass).text(data.appearance.weight[0])
+      $('.heroRace.' + heroClass).text(data.appearance.race)
+      $('.heroIntelligence.' + heroClass).text(data.powerstats.intelligence)
+      $('.heroSpeed.' + heroClass).text(data.powerstats.speed)
+      $('.heroStrength.' + heroClass).text(data.powerstats.strength)
+      $('.heroDurability.' + heroClass).text(data.powerstats.durability)
+      $('.heroPower.' + heroClass).text(data.powerstats.power)
+      $('.heroGroup.' + heroClass).text(data.connections.groupAffiliation)
+      $('.heroApp.' + heroClass).text(data.biography['firstAppearance'])
+      $(".heroImg." + heroClass).attr("data-heroId", heroId)
+      $(".heroImg." + heroClass).attr("src", data.image.url)
+      $(".heroWrapper." + heroClass).attr("data-name", data.name)
+      $(".heroWrapper." + heroClass).attr("data-heroId", heroId);
+  });
+
+  $.ajax({
+    method: 'get',
+    url: 'http://localhost:8080/percent',
+    contentType: "application/json",
+    data: {
+      heroId: heroId,
+    }
+  }).done(function(data){
+    console.log(data)
+  });
+};
